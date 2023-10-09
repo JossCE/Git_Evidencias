a8b52b02e2b017127755af2dcfe4b33126adb036
JossCE <josselin.cruz@uabc.edu.mx>
2023-10-08 19:19:30 -0700

    Segunda practica de python

---
+++ EjercicioPy2/README.md
@@ -0,0 +1,5 @@
+# CREANDO HISTORIAS
+
+Se pide al usuario introducir **palabras especificas**.
+
+El programa ordenara de acuerdo a lo establecido, las palabras ingresadas en los espacios correspondiente. Para crear una historia con las palabras escogidas por el usuario.
---
+++ EjercicioPy2/ejercicioPy2.py
@@ -0,0 +1,15 @@
+
+nombre = input("Ingresa un nombre: ")
+ciudad = input("Ingresa una ciudad: ")
+otraCiudad = input("Ingresa otra ciudad: ")
+transporte =input("Ingresa medio de transporte: ")
+actividad = input("Ingresa una actividad: ")
+clima = input("Ingresa un tipo de clima: ")
+
+   
+print ("******LA HISTORIA ES******")
+print ("El sueño de " + nombre + " era visitar la ciudad de " + ciudad + ".cd")
+print ("viajo en " + transporte + ", desde " + otraCiudad)
+print ("para poder hacer su actividad favorita: " + actividad + ",")
+print ("con el " + clima + " clima de la ciudad de " + ciudad + " y paisajes.")
+   
